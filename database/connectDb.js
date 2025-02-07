a8141ff1fc34a7b68fea18b9f7eb6d5d23d992b0
17e791u7 <ss8251342@gmail.com>
2025-02-07 16:54:03 +0530

    for db

--- database/connectDb.js
+++ database/connectDb.js
@@ -3,6 +3,6 @@ const url= "mongodb://127.0.0.1:27017/collegePortal"
 const live_url="mongodb+srv://bhaskarsharma9833:ram123@cluster0.h4bqw.mongodb.net/collegePortal?retryWrites=true&w=majority&appName=Cluster0" 
 
 const connectDb=()=>{
-    return mongoose.connect(url).then(()=>console.log("connected")).catch((error)=>console.log(error))
+    return mongoose.connect(live_url).then(()=>console.log("connected")).catch((error)=>console.log(error))
 } 
 module.exports=connectDb ;
